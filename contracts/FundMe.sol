9aca5ef56155662b01656ecad6a6d9a808a869d6
ThomasCode92 <thomascode92@hotmail.com>
2023-07-26 17:06:39 +0200

    Sending ETH to a Contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -7,7 +7,14 @@ pragma solidity ^0.8.8;
 //  - Set a minimum funding value in USD
 
 contract FundMe {
-    function fund() public {}
+    function fund() public payable {
+        // Want to be able to set a minimum fund amount in USD
+        //  1. How do we send ETH to this contract?
+        require(msg.value > 1e18, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
+
+        // What is reverting?
+        // undo any action before, and send remaining gas back
+    }
 
     function withdraw() public {}
 }
