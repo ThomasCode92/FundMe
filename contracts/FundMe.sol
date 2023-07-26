117666cb31f993c942bfb388bc309d51476a86a4
ThomasCode92 <thomascode92@hotmail.com>
2023-07-26 17:04:30 +0200

    Create FundMe Contract

---
+++ contracts/FundMe.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+// What this Contract should do:
+//  - Get funds from users
+//  - Withdraw funds
+//  - Set a minimum funding value in USD
+
+contract FundMe {
+    function fund() public {}
+
+    function withdraw() public {}
+}
