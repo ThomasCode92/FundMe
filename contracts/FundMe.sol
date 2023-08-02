d9dff6a4750613cf1523fa3b4e990d665bafb60f
ThomasCode92 <thomascode92@hotmail.com>
2023-08-02 18:07:19 +0200

    Libraries

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -6,9 +6,11 @@ pragma solidity ^0.8.8;
 //  - Withdraw funds
 //  - Set a minimum funding value in USD
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 public minimumUsd = 50;
 
     address[] public funders;
@@ -18,7 +20,7 @@ contract FundMe {
         // Want to be able to set a minimum fund amount in USD
         //  1. How do we send ETH to this contract?
         require(
-            getConversionRate(msg.value) >= minimumUsd * 1e18,
+            msg.value.getConversionRate() >= minimumUsd * 1e18,
             "Didn't send enough!"
         );
 
@@ -30,26 +32,4 @@ contract FundMe {
     }
 
     function withdraw() public {}
-
-    function getPrice() public view returns (uint256) {
-        // ABI      use an interface
-        // Address  0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        (, int256 answer, , , ) = priceFeed.latestRoundData(); // ETH in terms of USD
-
-        return uint256(answer * 1e10);
-    }
-
-    function getConversionRate(uint256 ethAmount)
-        public
-        view
-        returns (uint256)
-    {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // 1e18 == 1 * 10 ** 18 == 1000000000000000000
-
-        return ethAmountInUsd;
-    }
 }
