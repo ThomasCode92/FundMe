c3e3ff9cc9c025bd9a28dcbc7158709a15108456
ThomasCode92 <thomascode92@hotmail.com>
2023-09-06 07:33:54 +0200

    Hardhat Setup - Continued

---
+++ 02-Hardhat/contracts/FundMe.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+// What this Contract should do:
+//  - Get funds from users
+//  - Withdraw funds
+//  - Set a minimum funding value in USD
+
+import "./PriceConverter.sol";
+
+error NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 50;
+    address public immutable i_owner;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD * 1e18,
+            "Didn't send enough!"
+        );
+
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (uint256 funderIdx = 0; funderIdx < funders.length; funderIdx++) {
+            address funder = funders[funderIdx];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        (bool callSuccess, bytes memory dataReturned) = payable(msg.sender)
+            .call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
+
+        _;
+    }
+}
