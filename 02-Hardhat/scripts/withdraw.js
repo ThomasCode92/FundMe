f6176bdd53aa26739fc65f1e917e0dd5727bb21a
ThomasCode92 <thomascode92@hotmail.com>
2023-09-11 17:53:05 +0200

    Running Scripts on a Local Node

---
+++ 02-Hardhat/scripts/withdraw.js
@@ -0,0 +1,19 @@
+const { deployments, ethers } = require('hardhat');
+
+async function main() {
+  const fundMeDeployment = await deployments.get('FundMe');
+  const fundMe = await ethers.getContractAt('FundMe', fundMeDeployment.address);
+
+  console.log('Withdrawing contract...');
+  const transactionResponse = await fundMe.withdraw();
+
+  await transactionResponse.wait(1);
+  console.log('Withdrawn!');
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
