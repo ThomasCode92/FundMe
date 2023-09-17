350bc54f5138ffb02c3a1f201110b01b44d4c5dd
ThomasCode92 <thomascode92@hotmail.com>
2023-09-17 14:15:53 +0200

    Withdraw Function

--- 03-HTML-and-JS/main.js
+++ 03-HTML-and-JS/main.js
@@ -5,6 +5,7 @@ const CONTRACT_ADDRESS = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
 const connectBtnElement = document.getElementById('connect-btn');
 const fundBtnElement = document.getElementById('fund-btn');
 const balanceBtnElement = document.getElementById('balance-btn');
+const withdrawBtnElement = document.getElementById('withdraw-btn');
 const fundInputElement = document.getElementById('fund');
 
 async function connect() {
@@ -40,6 +41,24 @@ async function fund() {
   }
 }
 
+async function withdraw() {
+  console.log('Withdrawing...');
+  if (typeof window.ethereum !== 'undefined') {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+    try {
+      const transactionResponse = await contract.withdraw();
+
+      await listenForTransactionMine(transactionResponse, provider);
+      console.log('Done!');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
 async function getBalance() {
   if (typeof window.ethereum !== 'undefined') {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -65,4 +84,5 @@ function listenForTransactionMine(transactionResponse, provider) {
 
 connectBtnElement.addEventListener('click', connect);
 fundBtnElement.addEventListener('click', fund);
+withdrawBtnElement.addEventListener('click', withdraw);
 balanceBtnElement.addEventListener('click', getBalance);
